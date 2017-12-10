457d1f6b8c6b6f6d484b6b9349ad327201866415
Aprianto Wiwarsono <awiwarsono@gmail.com>
2017-12-10 13:59:30 +1300

    Update login authentication route

--- server/routes/authentication.js
+++ server/routes/authentication.js
@@ -6,9 +6,9 @@ const User = require('../../database/models/user');
 const { _secret } = require('../../config');
 
 // Helper functions
-const generateToken = (user) => {
+const generateToken = user => {
   return jwt.sign(user, _secret, { expiresIn: 10080 });
-}
+};
 
 router.post('/login', (req, res) => {
   console.log(req.body);
@@ -20,15 +20,20 @@ router.post('/signup', (req, res) => {
   const name = req.body.name;
   const password = req.body.password;
 
-  User.findOne({email}).then((user) => {
-    if (user) {
-      res.status(409).json({message: 'Email taken'});
-    } else {
-      User.create(/*Add user here*/).then((user) => {
-        c
-      }) 
-    }
-  })
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        res.status(409).json({ message: 'Email taken' });
+      } else {
+        User.create(/*Add user here*/).then(user => {
+          const token = generateToken(user);
+          res.status(201).json({ message: 'Registration succcessful', token });
+        });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: error });
+    });
 });
 
 module.exports.auth = router;
